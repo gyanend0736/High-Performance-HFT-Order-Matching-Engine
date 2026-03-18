ccd74fa7c0048248b1d13e9969ffdac4b61a3fba
Gyanendra singh <singhgyanendra715@gmail.com>
2026-03-18 18:51:54 +0530

    Fix C++ include paths and python-config for Render

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -o errexit
 # 1. Install Python packages
 pip install -r requirements.txt
 
-# 2. Compile the C++ engine for Linux (Notice the updated folder paths!)
-g++ -O3 -Wall -shared -std=c++17 -fPIC $(python -m pybind11 --includes) cpp_engine/src/binding.cpp cpp_engine/src/OrderBook.cpp -o trading_engine$(python-config --extension-suffix)
+# 2. Compile the C++ engine for Linux
+g++ -O3 -Wall -shared -std=c++17 -fPIC $(python -m pybind11 --includes) -Icpp_engine/include cpp_engine/src/binding.cpp cpp_engine/src/OrderBook.cpp -o trading_engine$(python -c "import sysconfig; print(sysconfig.get_config_var('EXT_SUFFIX'))")
